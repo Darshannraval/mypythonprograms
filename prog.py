60959f462f559215585f3f1e6850fa9feb3689ea
Darshannraval <35019331+Darshannraval@users.noreply.github.com>
2022-07-28 19:12:58 +0530

    Create prog.py

---
+++ prog.py
@@ -0,0 +1,8 @@
+import yaml
+
+with open("kitchen.yml") as f:
+    read_data = yaml.safe_load(f)
+    if read_data["provisioner"]["product_version"] == "17.9.46":
+      read_data["provisioner"]["product_version"] = "17.10.0"
+with open ("kitchen.yml", "w") as h:      
+    yaml = yaml.dump(read_data, h)
