c8f49c733189c5cfefcc8caad229f99e55dae73d
Darshannraval <35019331+Darshannraval@users.noreply.github.com>
2022-08-03 08:28:29 +0530

    fixed indentations

--- prog.py
+++ prog.py
@@ -4,5 +4,5 @@ with open("kitchen.yml") as f:
     read_data = yaml.safe_load(f)
     if read_data["provisioner"]["product_version"] == "17.9.46":
       read_data["provisioner"]["product_version"] = "17.10.0"
-with open ("kitchen.yml", "w") as h:      
-    yaml = yaml.dump(read_data, h)
+    with open ("kitchen.yml", "w") as h:      
+      yaml = yaml.dump(read_data, h)
