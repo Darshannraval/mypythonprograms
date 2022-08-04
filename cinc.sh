e3530190c1725c4a5a5be097b5b3d01dfce513c5
Darshannraval <35019331+Darshannraval@users.noreply.github.com>
2022-08-04 09:22:49 +0530

    optimized script

--- cinc.sh
+++ cinc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 os_name=$(cat /etc/os-release | awk -F '=' '/^NAME/{print $2}' | awk '{print $1}' | tr -d '"')
+os_versionid=$(cat /etc/os-release | awk -F '=' '/^VERSION_ID/{print $2}' | awk '{print $1}' | tr -d '"')
 
 case $os_name in
     "Ubuntu")
        echo "system is ubuntu"
-       os_versionid=$(cat /etc/os-release | awk -F '=' '/^VERSION_ID/{print $2}' | awk '{print $1}' | tr -d '"')
        if [ "$os_versionid" = "20.04" ]
        then
          wget http://downloads.cinc.sh/files/stable/cinc/17.10.0/ubuntu/20.04/cinc_17.10.0-1_amd64.deb
@@ -15,7 +15,6 @@ case $os_name in
     ;;
     "CentOS")
        echo "system is centos"
-       os_versionid=$(cat /etc/os-release | awk -F '=' '/^VERSION_ID/{print $2}' | awk '{print $1}' | tr -d '"')
        if [ "$os_versionid" = "7" ]
        then
          wget http://downloads.cinc.sh/files/stable/cinc/17.10.0/el/7/cinc-17.10.0-1.el7.x86_64.rpm
@@ -25,7 +24,6 @@ case $os_name in
     ;;  
     "Rocky")
        echo "system is rocky"
-       os_versionid=$(cat /etc/os-release | awk -F '=' '/^VERSION_ID/{print $2}' | awk '{print $1}' | tr -d '"')
        if [ "$os_versionid" = "8.5" ]
        then
          wget http://downloads.cinc.sh/files/stable/cinc/17.10.0/el/8/cinc-17.10.0-1.el8.x86_64.rpm
