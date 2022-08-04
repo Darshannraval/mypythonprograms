5bcb304935db4f3efb62e859c3702dd2dc5f9432
Darshannraval <35019331+Darshannraval@users.noreply.github.com>
2022-08-04 09:38:49 +0530

    modified variables

--- cinc.sh
+++ cinc.sh
@@ -2,10 +2,10 @@
 
 os_name=$(cat /etc/os-release | awk -F '=' '/^NAME/{print $2}' | awk '{print $1}' | tr -d '"')
 os_versionid=$(cat /etc/os-release | awk -F '=' '/^VERSION_ID/{print $2}' | awk '{print $1}' | tr -d '"')
+echo "system is $os_name"
 
 case $os_name in
     "Ubuntu")
-       echo "system is ubuntu"
        if [ "$os_versionid" = "20.04" ]
        then
          wget http://downloads.cinc.sh/files/stable/cinc/17.10.0/ubuntu/20.04/cinc_17.10.0-1_amd64.deb
@@ -14,7 +14,6 @@ case $os_name in
        fi
     ;;
     "CentOS")
-       echo "system is centos"
        if [ "$os_versionid" = "7" ]
        then
          wget http://downloads.cinc.sh/files/stable/cinc/17.10.0/el/7/cinc-17.10.0-1.el7.x86_64.rpm
@@ -23,11 +22,10 @@ case $os_name in
       fi
     ;;  
     "Rocky")
-       echo "system is rocky"
        if [ "$os_versionid" = "8.5" ]
        then
          wget http://downloads.cinc.sh/files/stable/cinc/17.10.0/el/8/cinc-17.10.0-1.el8.x86_64.rpm
          sudo rpm -e cinc
          sudo rpm -i cinc-17.10.0-1.el8.x86_64.rpm
        fi
-esac 
+esac
